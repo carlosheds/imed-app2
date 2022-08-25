7b19a0b5ebbbd1688941b012dc10cf481ed5f0ae
carlosheds <carlos.heds@gmail.com>
2022-08-24 22:58:10 -0300

    auto-formated code from the Drug class and added some files to gitignore

--- app/src/main/java/com/br/imed_app2/model/Drug.kt
+++ app/src/main/java/com/br/imed_app2/model/Drug.kt
@@ -2,15 +2,14 @@ package com.br.imed_app2.model
 
 import java.time.LocalDateTime
 
-class Drug (
+class Drug(
     val id: Int = 0,
-    val name : String,
-    val dosage : String,
-    val startTime : String,
-    val frequency : String
-        )
-{
-    val creationTimestamp : LocalDateTime
+    val name: String,
+    val dosage: String,
+    val startTime: String,
+    val frequency: String
+) {
+    val creationTimestamp: LocalDateTime
 
     init {
         creationTimestamp = LocalDateTime.now()
